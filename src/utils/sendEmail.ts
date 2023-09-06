748e7d9c61af0ed7bafcb6a3d8f0607c6bdaba51
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2023-09-06 11:36:50 -0700

    bug fix: hopefully fixed email

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,3 +1,24 @@
+import { z } from "zod"
+
+const resendErrorSchema = z.union([
+    z.object({
+        name: z.string(),
+        message: z.string(),
+        statusCode: z.number(),
+    }),
+    z.object({
+        name: z.literal('UnknownError'),
+        message: z.literal('Unknown Error'),
+        statusCode: z.literal(500),
+        cause: z.any(),
+    }),
+])
+type ResendError = z.infer<typeof resendErrorSchema>
+
+const resendSuccessSchema = z.object({
+    id: z.string(),
+})
+
 export async function sendEmail(email: {
     to: string
     subject: string
@@ -8,32 +29,52 @@ export async function sendEmail(email: {
     const resendAPIKey = process.env.RESEND_API_KEY
     if (!resendAPIKey) {
         console.error(`RESEND_API_KEY not set and we're not in mocks mode.`)
-		console.error(
-			`To send emails, set the RESEND_API_KEY environment variable.`,
-		)
-		console.error(`Would have sent the following email:`, JSON.stringify(email))
+        console.error(
+            `To send emails, set the RESEND_API_KEY environment variable.`,
+        )
+        console.error(`Would have sent the following email:`, JSON.stringify(email))
 
         return {
-			status: 'success',
-			data: { id: 'mocked' },
-		} as const
+            status: 'success',
+            data: { id: 'mocked' },
+        } as const
     }
-    try {
-        const res = fetch('https://api.resend.io/emails', {
-            method: 'POST',
-            body: JSON.stringify({
-                from,
-                ...email,
-            }),
-            headers: {
-                Authorization: `Bearer ${resendAPIKey}`,
-                'Content-Type': 'application/json',
-            },
-        })
-        return res
-    } catch (error) {
-        console.error('Error sending email:', error)
-        return error
-    }
+    const res = await fetch('https://api.resend.io/emails', {
+        method: 'POST',
+        body: JSON.stringify({
+            from,
+            ...email,
+        }),
+        headers: {
+            Authorization: `Bearer ${resendAPIKey}`,
+            'Content-Type': 'application/json',
+        },
+    })
+    const data: unknown = await res.json();
+    const parsedData = resendSuccessSchema.safeParse(data)
 
+	if (res.ok && parsedData.success) {
+		return {
+			status: 'success',
+			data: parsedData,
+		} as const
+	} else {
+		const parseResult = resendErrorSchema.safeParse(data)
+		if (parseResult.success) {
+			return {
+				status: 'error',
+				error: parseResult.data,
+			} as const
+		} else {
+			return {
+				status: 'error',
+				error: {
+					name: 'UnknownError',
+					message: 'Unknown Error',
+					statusCode: 500,
+					cause: data,
+				} satisfies ResendError,
+			} as const
+		}
+	}
 }
