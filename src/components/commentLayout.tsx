bb08a04ea6b13c876a030c73ae1b86193fb4e8bf
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2023-09-12 17:46:56 -0700

    updated package.json

--- src/components/commentLayout.tsx
+++ src/components/commentLayout.tsx
@@ -212,7 +212,7 @@ export function CommentLayout({ slug }: { slug: string }) {
                                                 setGotime(true);
                                             }}
                                         >
-✅
+                                            ✅
                                         </button>
                                     </div>
                                 ) : (
