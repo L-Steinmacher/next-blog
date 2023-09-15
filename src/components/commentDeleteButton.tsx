dd2068e10693c115bdfaf70df4d84763878f7fad
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2023-09-14 17:51:18 -0700

    delete button component

---
+++ src/components/commentDeleteButton.tsx
@@ -0,0 +1,38 @@
+import { api } from '~/utils/api';
+
+export default function CommentDeleteButton({
+    commentId,
+}: {
+    commentId: string;
+}) {
+    const utils = api.useContext();
+    const deleteComment = api.comments.deleteComment.useMutation({
+        onSuccess: async () => {
+            await utils.comments.invalidate();
+        },
+    });
+
+    function handleDeleteComment(commentId: string) {
+        return deleteComment.mutate(
+            { commentId },
+            {
+                onSettled: () => {
+                    console.log('Comment deleted');
+                },
+            },
+        );
+    }
+    return (
+        <>
+            <button
+                className="items-center rounded-md border border-transparent bg-none px-4 py-2 text-base font-medium text-white shadow-sm shadow-slate-400 hover:bg-red-200 focus:outline-none"
+                onClick={e => {
+                    e.preventDefault();
+                    handleDeleteComment(commentId);
+                }}
+            >
+                ❌
+            </button>
+        </>
+    );
+}
